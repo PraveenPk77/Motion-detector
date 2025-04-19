11f8334725f29e20716f7e6a56ed6049f20cc1cd
PraveenPk77 <2403717620521304@cit.edu.in>
2025-04-19 19:34:27 +0530

    Add files via upload

---
+++ MOTION DETECTION AND INSTANT ALERTER/motio/motio/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace motio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sender_mail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(27, 71);
+            this.videoSourcePlayer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(819, 431);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame_1);
+            this.videoSourcePlayer1.Click += new System.EventHandler(this.videoSourcePlayer1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(872, 112);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(315, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(872, 159);
+            this.startButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(176, 69);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(872, 246);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(176, 65);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(872, 330);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 20);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Alaram & Notify";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(997, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(867, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Select Source :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(484, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "MOTION DETECTION AND INSTANT ALERTER";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(28, 546);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(792, 56);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 2;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(29, 518);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sensitivity";
+            // 
+            // sender_mail
+            // 
+            this.sender_mail.Location = new System.Drawing.Point(888, 415);
+            this.sender_mail.Margin = new System.Windows.Forms.Padding(4);
+            this.sender_mail.Name = "sender_mail";
+            this.sender_mail.Size = new System.Drawing.Size(299, 22);
+            this.sender_mail.TabIndex = 10;
+            this.sender_mail.Text = "kgmpraveen123@gmail.com";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(883, 375);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Mail Id:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10"});
+            this.comboBox2.Location = new System.Drawing.Point(1027, 465);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(883, 466);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Modem port";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(883, 518);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Phone No:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(888, 546);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(299, 22);
+            this.textBox1.TabIndex = 15;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(876, 592);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(107, 20);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = " Notify E-mail";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(1067, 592);
+            this.checkBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(105, 20);
+            this.checkBox3.TabIndex = 17;
+            this.checkBox3.Text = "Notify Phone";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1036, 330);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 69);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Test Sound";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(835, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1236, 642);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sender_mail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox sender_mail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ MOTION DETECTION AND INSTANT ALERTER/motio/motio/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using System.IO.Ports;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace motio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        // Configure the serial port for communication with the GSM modem
+      
+
+        FilterInfoCollection devices;
+        VideoCaptureDevice camera = new VideoCaptureDevice();
+
+        MotionDetector detector;
+        float detectionLevel,sensitivity;
+        
+        BlobCountingObjectsProcessing motionProcessing = new BlobCountingObjectsProcessing();
+        private SerialPort serialPort;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sensitivity = 0.02f;
+            detector = new MotionDetector(new TwoFramesDifferenceDetector(), new BlobCountingObjectsProcessing());
+            
+            detectionLevel = 0;
+
+            devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo d in devices)
+            {
+                comboBox1.Items.Add(d.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            // Configure the serial port for communication with the GSM modem
+           
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //timer1.Enabled = true;
+            if (camera.IsRunning == true)
+            {
+                camera.Stop();
+            }
+
+            camera = new VideoCaptureDevice(devices[comboBox1.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = camera;
+            videoSourcePlayer1.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            detectionLevel = 0;
+            videoSourcePlayer1.SignalToStop();
+            camera.Stop();
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           // if ( checkBox1.Checked == true)
+                if (detectionLevel > 0 && checkBox1.Checked == true)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                player.SoundLocation = "sound.wav";
+                player.Play();
+                MessageBox.Show("motionProcessing");
+                Bitmap bitmap = videoSourcePlayer1.GetCurrentVideoFrame();
+                bitmap.Save(Application.StartupPath + "/" + textBox2.Text + "image.jpg");
+                textBox2.Text = textBox2.Text + 1;
+                // checkBox1.Checked = false;
+                detectionLevel = 0;
+                SendEmailNotification();
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (camera.IsRunning == true)
+            {
+                detectionLevel = 0;
+                videoSourcePlayer1.SignalToStop();
+                camera.Stop();
+            }
+        }
+
+        private void videoSourcePlayer1_NewFrame_1(object sender, ref Bitmap image)
+        {
+            // detectionLevel = detector.ProcessFrame(image);
+            if (detector.ProcessFrame(image) > sensitivity)
+            {
+                detectionLevel = detector.ProcessFrame(image);
+                //////MessageBox.Show("motionProcessing" );
+                notify();
+            }
+        }
+        private void notify()
+        {
+            if (checkBox1.Checked == true)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                player.SoundLocation = "sound.wav";
+                player.Play();
+                MessageBox.Show("motionProcessing");
+                Bitmap bitmap = videoSourcePlayer1.GetCurrentVideoFrame();
+                bitmap.Save(Application.StartupPath + "/" + textBox2.Text + "image.jpg");
+                //textBox2.Text = textBox2.Text + 1;
+                // checkBox1.Checked = false;
+                detectionLevel = 0;
+                SendEmailNotification();
+            }
+        }
+
+        private void videoSourcePlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if(trackBar1.Value == 1 )
+            {
+                sensitivity = 0.01f;
+            }
+            else if (trackBar1.Value == 2)
+            {
+                sensitivity = 0.02f;
+            }
+            else if (trackBar1.Value == 3)
+            {
+                sensitivity = 0.03f;
+            }
+            else if (trackBar1.Value == 4)
+            {
+                sensitivity = 0.04f;
+            }
+            else if (trackBar1.Value == 5)
+            {
+                sensitivity = 0.05f;
+            }
+            else if (trackBar1.Value == 6)
+            {
+                sensitivity = 0.06f;
+            }
+            else if (trackBar1.Value == 7)
+            {
+                sensitivity = 0.07f;
+            }
+            else if (trackBar1.Value == 8)
+            {
+                sensitivity = 0.08f;
+            }
+            else if (trackBar1.Value == 9)
+            {
+                sensitivity = 0.09f;
+            }
+        }
+        public  void SendEmailNotification()
+
+        {
+            DateTime now = DateTime.Now;
+            string time = now.ToString();
+            // Sender credentials
+            string senderEmail = "bsvssanthiya9@gmail.com";
+            string senderPassword = "rbsaxzkxihgqhuao";
+
+            // Recipient email address
+            string recipientEmail = "chenningiri8889@gmail.com";
+            // recipientEmail = tostrin(sender_mail.Text);
+            Attachment att = new Attachment(Application.StartupPath + "/" + textBox2.Text + "image.jpg");
+            // SMTP client configuration
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential(senderEmail, senderPassword);
+
+            // Email message
+            MailMessage mailMessage = new MailMessage(senderEmail, recipientEmail);
+            mailMessage.Subject = "Motion Detected!";
+            mailMessage.Body = "Motion has been detected by the security system. @ " + time;
+            mailMessage.Attachments.Add(att);
+            client.Send(mailMessage);
+            MessageBox.Show("email sent");
+            Console.WriteLine("Email notification sent.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendEmailNotification();
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+            player.SoundLocation = "sound.wav";
+            player.Play();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        static void CallUserPhone()
+        {
+          SerialPort serialPort = new SerialPort("COM3", 9600);
+            serialPort.Open();
+            // Send AT command to make a call
+            serialPort.WriteLine("ATD+1234567890;"); // Replace +1234567890 with the user's phone number
+           
+        }
+    }
+    }
